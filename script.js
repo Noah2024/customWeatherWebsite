ff785b68169855fa3c930a9b99ab2a317c5e6e74
Noah2024 <139483586+Noah2024@users.noreply.github.com>
2025-04-06 20:26:09 -0400

    AddedLocationRequest

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const button = document.getElementById('apiRequest');
+let lat;
+let long;
+
+function reqeustWeatherData(lat, long){
+    
+}
+
+function locationSuccess(postion){
+    lat = postion.coords.latitude;
+    long = postion.coords.longitude;
+    console.log("Latitude: " + lat +  ", Longitude: " + long);
+    return lat, long;
+}
+
+function requestLocationData(){
+    console.log("Requesting Location Data from User");
+    if (navigator.geolocation){
+        let location = navigator.geolocation.getCurrentPosition(locationSuccess);
+        console.log("Location Recieved", location);
+        return location;
+    }
+}
 
 console.log(button);
 button.addEventListener('click', ()=>{
     console.log("API Request Initiated");
+    requestLocationData();
 })
