3a69316a77c3b11a0105f809e3c3f400e3f4b494
Noah2024 <139483586+Noah2024@users.noreply.github.com>
2025-04-08 15:42:34 -0400

    ContentFormatted

--- loadFormatData.js
+++ loadFormatData.js
@@ -6,12 +6,18 @@ const numBoxes = 6;
 
 function loadWeatherData(data){
     console.log(weatherBoxes);
-    for (let i = 0; i < numBoxes + day; i++){
-        console.log(data.properties.periods[i]);
-        weatherBoxes[boxIndex].innerHTML = i;
-        weatherBoxes[boxIndex].innerHTML += '"\n <h> TEST <h> \n TEST 2"';
+    for (let i = 0; i < numBoxes; i++){
+        curtTime = data.properties.periods[i]
+        weatherBoxes[i].innerHTML = `
+        <h2> ${curtTime.name} </h2>
+        <img src = "${curtTime.icon}", alt="Weather icon"> > 
+        <p> Temperature: ${curtTime.temperature} ${curtTime.temperatureUnit}</p>
+        <p> Wind Speed: ${curtTime.windSpeed} </p>
+        <p> Wind Direction: ${curtTime.windDirection} </p>
+        `; 
     }
 }
+//p> ${curtTime.detailedForecast}/<p>
 
 document.addEventListener('DOMContentLoaded', () => {
     const weatherData = JSON.parse(sessionStorage.getItem("weatherData"));
