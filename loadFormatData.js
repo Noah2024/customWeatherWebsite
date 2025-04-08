a8e0100ecfac62fc480631999fe8de50af274305
Noah2024 <139483586+Noah2024@users.noreply.github.com>
2025-04-08 14:59:01 -0400

    DataPassedSuccessfully

--- loadFormatData.js
+++ loadFormatData.js
@@ -1,7 +1,21 @@
 const today = new Date();
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weatherBoxes = document.querySelectorAll(".weatherBox");
+const day = today.getDay();
+const numBoxes = 6;
+
+function loadWeatherData(data){
+    console.log(weatherBoxes);
+    for (let i = 0; i < numBoxes + day; i++){
+        console.log(data.properties.periods[i]);
+        weatherBoxes[boxIndex].innerHTML = i;
+        weatherBoxes[boxIndex].innerHTML += '"\n <h> TEST <h> \n TEST 2"';
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const weatherData = JSON.parse(sessionStorage.getItem("weatherData"));
     console.log(weatherData)
+    console.log(daysOfWeek[today.getDay()]);
+    loadWeatherData(weatherData);
 })
